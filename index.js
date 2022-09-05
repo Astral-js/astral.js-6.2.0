45e26b01bc815c4a536b5f054f7ebe5f10e7a517
Arizaki <79724270+Oreki-ASTRAL@users.noreply.github.com>
2022-09-05 15:08:18 +0200

    v6.2.0

---
+++ index.js
@@ -0,0 +1,77 @@
+const astral = require('astral.js@4.6.0')
+const db = require('astral.ts@2.3.0')
+
+astral.config{
+  Token: "Token de votre application",
+  Prefix: "Prefix de votre application",
+  Key: "Clef unique"
+}
+
+///Package usage
+astral.open.gl
+astral.on 
+astral.login
+astral.terminal
+astral.debug
+astral.disconnect
+astral.asci
+///
+
+
+astral.bot{
+  Nom: "ping",
+  code: `As${titre; "Ping"} As${description; "la latence du bot est de {As$Integer; "Cmd.ping"}"`,
+  Intégration: "Cmd.ping"
+}
+
+astral.bot{
+  Nom: "ban",
+  code: `As${ban; {As$userPing};
+  As${return; {As$message} }}
+  
+  As${title; "Bannissement} As${description; {As$userPing} à été banni du serveur."`,
+  Intégration: "Cmd.log;Cmd.ban;Cmd.return;Cmd.on;Cmd.db"
+}
+
+astral.bot{
+  Nom: "Reboot",
+  Code: "As${Cmd.restart; Cmd.endo "/stop"; Cmd.block "false"; Cmd.node "node index.js"
+  As${titre "Restart du bot"} As${description "le redémarrage du bot est effectué !"},
+  Intégration: "Cmd.restart;Cmd.endo;Cmd.block;Cmd.node"
+}
+
+astral.registre(true; console.id"")//true = entre les "" mettre un id de salon | false écrit tout dans la base de données et dans la console.
+
+astral.client(token.connect)
+
+astral.key //Ne pas toucher la suite, cela fait partie de l'environnement de la Key.
+
+astral.registre("key.dll");
+astral.login('0_x_09__01ANPQMX-N09NAZ245x')
+astral.ts('db.connect');
+
+db.on{
+  Login: "",
+  Password: ""
+} //Créé vous un compte sur https://astral.netlify.app/db
+
+Integer: //Juste la pour décoré..
+
+astral.start{
+better.astral require('db.astral.key')
+}
+
+create.db.new('Login_Password_0x_lds-_')
+astral.include.registre("terminal.js")
+
+Terminal.login('Login_Password')
+
+html.port("open")
+Connexion.local_host{
+  Name: "Terminal",
+  Protocole: "80",
+  Api: "astral.ts",
+  local: "website.url" //changer la value par le site web de votre bot ou le local host.
+}
+///fin
+
