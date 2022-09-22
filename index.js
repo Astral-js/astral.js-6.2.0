88832fe0f0f8594a493c9cbb95b07cf5252947d7
Arizaki <79724270+Oreki-ASTRAL@users.noreply.github.com>
2022-09-22 11:59:52 +0200

    Update 22 september 2022

--- index.js
+++ index.js
@@ -19,6 +19,27 @@ astral.asci
 
 
 astral.bot{
+const astral = require('astral.js@6.2.0')
+const db = require('astral.ts@2.3.0')
+
+astral.config{
+  Token: "Token de votre application",
+  Prefix: "Prefix de votre application",
+  Key: "Clef unique"
+}
+
+///Package usage
+astral.open.gl
+astral.on 
+astral.login
+astral.terminal
+astral.debug
+astral.disconnect
+astral.asci
+///
+
+
+astral.bot{
   Nom: "ping",
   code: `As${titre; "Ping"} As${description; "la latence du bot est de {As$Integer; "Cmd.ping"}"`,
   Intégration: "Cmd.ping"
@@ -75,3 +96,62 @@ Connexion.local_host{
 }
 ///fin
 
+astral.tmc //terminal config
+astral.tmc.color{"./tmc.json"}
+astral.tmc.start{"astral-tmc"} //ce code sera celui que vous entrerez dans votre console pour lancer le terminal astral.
+
+astral.tmc.customize{"./tmc.json"}
+
+astral.tmc.return //vous retourne toute les infos sur astral et votre machine en exécutant /return dans le terminal.
+
+astral.tmc.codex //Ne pas toucher
+
+
+//astral.js pour Facebook, Instagram, WhatsApp, Messenger, Snapchat,..
+
+const astral = require("astral.js")
+const db = require("astral.ts@2.3.0")
+const dev = require("astral.js@dev4.0")
+
+const astral.start {
+  Token: "",
+  Api: ""
+  //fichier complètement customisable (voir https://astral.be) 
+}
+
+  const import = require("astral.db")
+const module = require('https://astral.db.netlify.app/cod/login/${Username;Password}')
+
+astral.connect
+astral.sendTest
+astral.return.console
+astral.check.config
+astral.check.key
+
+let object {
+  astral: "6.2.0R",
+  key: "0x__xNPSQKLMVCDR__0x",
+  key.check: "key.connect"
+}
+
+connect.console.undo
+terminal.config
+
+///Espace dédié au commandes.
+astral.commande{
+  nom: "connect",
+  code: `As${function; "Cmd.connect"} As${return; "Votre code à bien été injecté.. Merci de patienter. cette opération peut prendre jusqu'à 2 min celon votre connexion.."} As${app; "Bienvenue sur $App propulsé par Astral."} `,
+  Intégration: "Cmd.connect;Cmd.return;Cmd.app;Cmd.Ping",
+  if: ("return; $wait{$injection}")
+}
+
+  ///fin de la partie commande.
+
+///Partie configuration terminal / console
+
+astral.tmc.execute
+astral.tmc.open
+astral.tmc.config
+astral.open.config //Vous permet de configurer le terminal même en pleine utilisation (Nouveautée)
+
+tmc.config("./config.json")
